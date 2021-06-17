6cb690c9cc648bc149656f556fc67bd719a48eba
guilherme <guilherme.alves96@hotmail.com>
2021-06-16 23:15:12 -0300

    Deixando os nomes dos produtos dinamicos

--- gkapp/views.py
+++ gkapp/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    produtos = {
+        1: 'Chaleira esmaltada Cupcake',
+        2: 'Biscuit',
+        3: 'Chaleira esmaltada Porquinho',
+    }
+    dados = {
+        'nome_dos_produtos': produtos
+    }
+    return render(request, 'index.html', dados)
 
 
 def produto(request):
