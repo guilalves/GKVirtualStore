f51583bbc62df194c7164b707364add6da164427
guilherme <guilherme.alves96@hotmail.com>
2021-06-17 18:53:21 -0300

    Inserindo parametros na URL

--- gkapp/models.py
+++ gkapp/models.py
@@ -3,10 +3,9 @@ from datetime import datetime
 from django.db import models
 
 class Produto(models.Model):
-    # data = datetime.now() TODO testar
-    # data_de_publicacao = data.strftime('%d/%m/%Y') TODO testar
-    data_de_publicacao = models.DateTimeField(default=datetime.now, blank=True)
-    nome_do_produto = models.CharField(max_length=200)
+    data_de_publicacao = models.DateTimeField(default=datetime.now(), blank=True)
+    informacoes = models.TextField(max_length=200)
     modelo = models.CharField(max_length=50)
+    nome_do_produto = models.CharField(max_length=200)
     preco = models.CharField(max_length=20)
-    tamanho = models.CharField(max_length=50)
+    tamanho = models.CharField(max_length=100)
--- gkapp/views.py
+++ gkapp/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import  get_list_or_404, get_object_or_404, render
+
+from .models import Produto
 
 
 def index(request):
-    produtos = {
-        1: 'Chaleira esmaltada Cupcake',
-        2: 'Biscuit',
-        3: 'Chaleira esmaltada Porquinho',
-    }
+    produtos = Produto.objects.all()
+
     dados = {
-        'nome_dos_produtos': produtos
+        'produtos': produtos
     }
     return render(request, 'index.html', dados)
 
 
-def produto(request):
-    return render(request, 'produto.html')
+def produto(request, produto_id):
+    try:
+        produto = get_object_or_404(Produto, pk=produto_id)
+        produto_a_exibir = {
+                'produto': produto
+        }
+    except Exception:
+        print('ERROR! Nao foi possivel obter as informacoes dos produtos.')
+
+    return render(request, 'produto.html', produto_a_exibir)
