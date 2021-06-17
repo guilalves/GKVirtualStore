8be9b7393d2b31605ad6046d21c40c25f9cbc1ff
guilherme <guilherme.alves96@hotmail.com>
2021-06-17 00:18:23 -0300

    Migrando as tabelas e criando classe de modelo

--- gkapp/models.py
+++ gkapp/models.py
@@ -1,1 +1,12 @@
-# from django.db import models
+from datetime import datetime
+
+from django.db import models
+
+class Produto(models.Model):
+    # data = datetime.now() TODO testar
+    # data_de_publicacao = data.strftime('%d/%m/%Y') TODO testar
+    data_de_publicacao = models.DateTimeField(default=datetime.now, blank=True)
+    nome_do_produto = models.CharField(max_length=200)
+    modelo = models.CharField(max_length=50)
+    preco = models.CharField(max_length=20)
+    tamanho = models.CharField(max_length=50)
